2ca1d11263502254a0e9aacc9eb4670484f764fb
Trey Tomes <trey.tomes@gmail.com>
2016-01-26 09:48:55 -0600

    Split KeyboardListener into separate class files.

---
+++ Terminal/RawKeyEventArgs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Terminal
+{
+	/// <summary>
+	/// Raw KeyEvent arguments.
+	/// </summary>
+	public class RawKeyEventArgs : EventArgs
+	{
+		/// <summary>
+		/// VKCode of the key.
+		/// </summary>
+		public readonly int VKCode;
+
+		/// <summary>
+		/// WPF Key of the key.
+		/// </summary>
+		public readonly Key Key;
+
+		/// <summary>
+		/// Is the hitted key system key.
+		/// </summary>
+		public readonly bool IsSysKey;
+
+		/// <summary>
+		/// Unicode character of key pressed.
+		/// </summary>
+		public readonly string Character;
+
+		/// <summary>
+		/// Create raw keyevent arguments.
+		/// </summary>
+		public RawKeyEventArgs(int vkCode, bool isSysKey, string character)
+		{
+			VKCode = vkCode;
+			IsSysKey = isSysKey;
+			Character = character;
+			Key = KeyInterop.KeyFromVirtualKey(vkCode);
+		}
+
+		/// <summary>
+		/// Convert to string.
+		/// </summary>
+		/// <returns>Returns string representation of this key, if not possible empty string is returned.</returns>
+		public override string ToString()
+		{
+			return Character;
+		}
+	}
+}
